859f08a092123af8bb1b92333414a3ba295cfe28
Darshan Shetty <darshanshetty253101@gmail.com>
2022-09-23 13:52:26 +0530

    updated fibonacci

--- fibonacci.java
+++ fibonacci.java
@@ -3,6 +3,7 @@ class fibonacci {
         int n1 = 0, n2 = 1, n3, i, count = 10;
         System.out.println(n1 + "\n" + n2);
 
+        // we use 2 as 0 and 1 are aldready printed.
         for (i = 2; i < count; ++i) {
             n3 = n1 + n2;
             System.out.println("" + n3);
